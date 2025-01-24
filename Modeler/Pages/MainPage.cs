5ec2f3b426474460a6b08f2eb3470f9cee43ff09
吴冰 <wubing@xuanit.com>
2025-01-24 14:57:54 +0800

    add: 1. 添加EditForm组件

---
+++ Modeler/Pages/MainPage.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace Modeler.Pages;
+
+public class MainPage : ComponentBase
+{
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        base.BuildRenderTree(builder);
+
+        int sequence = 0;
+
+        builder.OpenElement(sequence++, "h3");
+        builder.AddContent(sequence++, "main page");
+        builder.CloseElement();
+        
+        builder.OpenComponent<StarshipPlainForm>(sequence);
+        builder.CloseComponent();
+    }
+}
